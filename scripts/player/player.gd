2be62baa755e1ce12b0afb46c8716661d6a59489
Kyle Jardine <kjardine00@gmail.com>
2024-07-01 15:07:53 -0400

    lvl 1 progress

--- scripts/player/player.gd
+++ scripts/player/player.gd
@@ -10,7 +10,7 @@ class_name Player
 @onready var grab_position = $GrabPosition
 
 const SPEED = 130
-const JUMP_VELOCITY = -290
+const JUMP_VELOCITY = -300
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
